c42c67bea9d8e1ebb53d5b676d0bb0485bfcb88c
brianngumbau <brianngumbau9075@gmail.com>
2024-02-03 01:33:15 -0800

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# Installs flask from pip3
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# install_flask.pp
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin'],
+  require => Package['python3-pip'],
 }
